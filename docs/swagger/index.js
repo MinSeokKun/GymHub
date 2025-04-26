747831163655d5f54ce194ee9ee3818680fe9a90
MinSeokKun <kimms51567115@gmail.com>
2025-04-26 13:22:23 +0900

    swagger 문서 추가

--- docs/swagger/index.js
+++ docs/swagger/index.js
@@ -15,16 +15,20 @@
  *     description: 헬스장 등록 및 관리
  *   - name: 회원 관리
  *     description: 헬스장 회원 등록 및 관리
+ *   - name: 인증
+ *     description: 사용자 인증 및 계정 관리
  */
 
 // 스키마 정의들을 가져옵니다
 const coreSchemas = require('./schemas/core');
 const memberSchemas = require('./schemas/members');
+const authSchemas = require('./schemas/auth');
 
 // API 경로 정의들을 가져옵니다
 const coreUsersPaths = require('./paths/core-users');
 const gymsPaths = require('./paths/gyms');
 const memberPaths = require('./paths/members');
+const authPaths = require('./paths/auth');
 
 // 전체 Swagger 스펙을 구성합니다
 const swaggerDefinition = {
@@ -53,16 +57,30 @@ const swaggerDefinition = {
       name: '회원 관리',
       description: '헬스장 회원 등록 및 관리',
     },
+    {
+      name: '인증',
+      description: '사용자 인증 및 계정 관리',
+    },
   ],
   paths: {
     ...coreUsersPaths,
     ...gymsPaths,
     ...memberPaths,
+    ...authPaths,
   },
   components: {
     schemas: {
       ...coreSchemas,
       ...memberSchemas,
+      ...authSchemas,
+    },
+    securitySchemes: {
+      bearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'JWT 토큰을 사용한 인증. \'Bearer\' 다음에 공백과 토큰을 입력하세요.',
+      },
     },
   },
 };
